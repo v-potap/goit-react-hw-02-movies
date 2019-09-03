bb7b3999ef1c2367ede025967abbfa255121d58f
v-potap <v.o.potapenko@gmail.com>
2019-09-03 10:17:31 +0300

    Updates

---
+++ precache-manifest.25a9d44a6bb7ae27bf34ff395f4a67ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9da0ed94dd3cac2be4c7e0cc0b00e83",
+    "url": "/goit-react-hw-02-movies/index.html"
+  },
+  {
+    "revision": "53a86a16c8f1fa9f8a14",
+    "url": "/goit-react-hw-02-movies/static/css/main.4f87a105.chunk.css"
+  },
+  {
+    "revision": "5adffcaf9acbd0defa95",
+    "url": "/goit-react-hw-02-movies/static/js/2.8eea4857.chunk.js"
+  },
+  {
+    "revision": "53a86a16c8f1fa9f8a14",
+    "url": "/goit-react-hw-02-movies/static/js/main.533843e2.chunk.js"
+  },
+  {
+    "revision": "ce5596951aecabfed635",
+    "url": "/goit-react-hw-02-movies/static/js/runtime~main.7b873d09.js"
+  }
+]);
